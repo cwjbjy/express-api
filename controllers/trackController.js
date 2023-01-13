d14dc4af8754ed8dc4d1eb4368bb66df1fddfaf5
cwj <595842570@qq.com>
2023-01-13 20:39:50 +0800

    行为收集接口

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -1,15 +1,18 @@
 const mysql = require("../db/mysql");
 const { CODE_SUCCESS } = require("../util/constants");
 
-const userList = [];
-
 function trackWeb(req, res) {
-  const { data } = req.body;
-  userList.push(JSON.stringify(data));
-  console.log(userList);
-  res.json({
-    code: CODE_SUCCESS,
-  });
+  const params = JSON.parse(req.query.data);
+  const { vs, deviceType, url, referer, localTime, delay } = params;
+  mysql
+    .query(
+      `INSERT INTO TRACK (vsManage,deviceType,currentUrl,refererUrl,userTime,delayTime) VALUES ('${vs}','${deviceType}','${url}','${referer}','${localTime}','${delay}');`
+    )
+    .then(() => {
+      res.json({
+        code: CODE_SUCCESS,
+      });
+    });
 }
 
 module.exports = {
